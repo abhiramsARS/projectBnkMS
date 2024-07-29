a9eb92ff9cbe23c4a8cf0d896e1f410d5fdeb188
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-07-29 22:51:42 +0530

    Add files via upload

---
+++ BankMS_MySQL.sql
@@ -0,0 +1,13 @@
+/* Creating Database */
+Create Database bankms;
+
+/* Using Database */
+use bankms;
+
+/* Creating Tables */
+create table account(ac_no varchar(9) not null primary key,ac_holder varchar(20) not null,ph_no varchar(12) not null,ac_balance int,ac_status varchar(2) not null,ac_date date);
+create table transaction(trans_id varchar(8) not null primary key,trns_date date not null,trans_ac varchar(9) not null,trans_method varchar(10) not null,amount integer not null ,trans_type varchar(2) not null);
+
+/* Adding Trial Data to tables */
+insert into account values("BAC-00001","Test","1234567890",0,'O',curdate());
+insert into transaction values("2420001",curdate(),"BAC-00001","deposit",0,"c");
